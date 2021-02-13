d37000226cc0d1ac7762ae095b25384f83184939
Hyun <hyun@balancehero.com>
2021-02-13 17:40:02 +0900

    study

--- src/main/python/exception.py
+++ src/main/python/exception.py
@@ -7,9 +7,11 @@ except IOError as err:
     print(f"Error: can\'t find file or read data {err}")
 except(AssertionError, TypeError):
     print("handle multiple error")
-except:
-    print("any Exception")
+except Exception as e:
+    print(f"any Exception : {e}")
     raise  # raise same error again after printing
+except :
+    print(f"any Exception 2nd way without parameter")
 else:
     print("Written content in the file successfully")
     fh.close()
