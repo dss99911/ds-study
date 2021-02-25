ce04d8563030720c2a12a1b779b0299593b1dff8
Hyun <hyun@balancehero.com>
2021-02-25 20:57:27 +0900

    add

--- script/spark_submit.sh
+++ script/spark_submit.sh
@@ -8,6 +8,9 @@ DATA=$2
 
 /usr/lib/spark/bin/spark-submit \
 --properties-file ${PROPERTIES_FILE} \
---class com.MainClass
---driver-java-options "-Dconfig.resource=hyun.conf"
+--class com.MainClass \
+--master "master-url" \
+--deploy-mode "deploy-mode" \
+--conf key=value \
+--driver-java-options "-Dconfig.resource=application-dev.conf" \
 ${EXEC_JAR} "${DT}" "${DATA}"
