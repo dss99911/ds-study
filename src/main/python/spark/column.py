5f43261c79ea72e48a15873da960075666ff2230
Hyun <hyun@balancehero.com>
2021-08-10 13:23:12 +0900

    update

--- src/main/python/spark/column.py
+++ src/main/python/spark/column.py
@@ -4,3 +4,9 @@ df_matched.withColumn('pattern_group_num',F.lit(iter_num))
 
 #remove column
 df_others.drop('selected_msg')
+
+
+#select column
+c = ["b", "c"]
+d = ["d", "e"]
+df.select("a", *c, *d)
