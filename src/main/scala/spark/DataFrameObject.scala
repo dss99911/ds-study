44d669f74f51c7f1aab81b2b80d8737b59fbc2e1
Hyun <hyun@balancehero.com>
2021-11-15 10:56:29 +0900

    update

--- src/main/scala/spark/DataFrameObject.scala
+++ src/main/scala/spark/DataFrameObject.scala
@@ -21,6 +21,7 @@ class DataFrameObject {
     .withColumn("dt", typedLit(Seq(1, 2, 3)))
     .withColumn("item", element_at($"array_column", 1))//get item of an index. index starts with 1. udf에서 Array로 리턴했을 때 Array 값이 됨.
     .withColumn("item", $"sequence._1")//Sequence값인 경우, sequence의 필드명을 넣으면됨 _1, _2
+    .map { case Row(a: Long, b: String) => b.split("\\|").map(part => (a, part)) } // row변환 해서, 다른 row로 만들기. 타입이 명확해야지 에러가 안남. _1, _2 등의 column으로 설정됨, 어떻게 컬럼명을 쉽게 설정할지 고민 필
 
   /**
    * object를 컬럼에 넣기
