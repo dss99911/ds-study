ce0e22f565369ed07e72c283f0c1b59e693d072d
Hyun <hyun@balancehero.com>
2022-05-13 13:32:00 +0900

    update

--- python/spark/util/interactive.py
+++ python/spark/util/interactive.py
@@ -6,9 +6,8 @@ try:
     #intellij doesn't recognize spark instance on jupyter notebook.
     #this will occur error. but, intellij will recognize spark variable
     # noinspection PyUnresolvedReferences,PyUnboundLocalVariable
-    spark: SparkSession = spark
+    spark: SparkSession = __main__.global_dict["spark"]  # for livy session
+    spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
+    sc = spark.sparkContext
 except:
     pass
-
-spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
-sc = spark.sparkContext
